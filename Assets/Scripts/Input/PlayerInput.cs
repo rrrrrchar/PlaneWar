e81ce0bc4e9fbf0bd453d8551e5b82b4db156921
rrrrrchar <y1258182408@outlook.com>
2022-05-18 00:43:01 +0800

    玩家移动

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+//玩家输入动作表管理类，
+//inputsystem信号的接收与
+//发送事件给player
+[CreateAssetMenu(menuName = "Player Input")]
+public class PlayerInput : ScriptableObject, PlayerInputActions.IGamePlayActions
+{
+    //动作表的生成类
+    PlayerInputActions inputActions;
+
+    //初始化时给空委托
+    // 移动和停止移动事件
+    public event UnityAction<Vector2> OnMoveEvent = delegate { };
+    public event UnityAction OnStopMoveEvent = delegate { };
+    private void OnEnable()
+    {
+        //初始化
+        inputActions = new PlayerInputActions();
+        //登记回调函数
+        inputActions.GamePlay.SetCallbacks(this);
+
+    }
+    private void OnDisable()
+    {
+        DisableGamePlayInput();
+    }
+
+    //方便其他类启用与关闭动作表
+    public void EnableGamePlayInput()
+    {
+        //
+        inputActions.GamePlay.Enable();
+        //隐藏鼠标
+        Cursor.visible = false;
+    }
+    public void DisableGamePlayInput()
+    {
+        inputActions.GamePlay.Disable();
+    }
+
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //按住按键时调用 onMove事件
+        if(context.phase ==InputActionPhase.Performed)
+        {
+            OnMoveEvent.Invoke( context.ReadValue<Vector2>() );
+        }
+        //松开按键时调用 onStopMove事件
+        if(context.phase ==InputActionPhase.Canceled)
+        {
+            OnStopMoveEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/PLayer/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//玩家类
+//通过接收 playerInput发送过来的事件 与 刚体一起实现移动
+
+//添加2D刚体
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] PlayerInput input;
+    new Rigidbody2D rigidbody;
+
+    [SerializeField]float movespeed = 10f;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rigidbody.gravityScale = 0f;
+        //激活动作表
+        input.EnableGamePlayInput();
+    }
+
+    //订阅事件方便 移动/停止移动的实现
+    private void OnEnable()
+    {
+        input.OnMoveEvent += MoveFunc;
+        input.OnStopMoveEvent += StopMoveFunc;
+    }
+    //取消订阅
+    private void OnDisable()
+    {
+        input.OnMoveEvent -= MoveFunc;
+        input.OnStopMoveEvent -= StopMoveFunc;
+    }
+
+    void MoveFunc(Vector2 moveinput)
+    {
+        //Vector2 moveAmount = moveinput * movespeed;
+        rigidbody.velocity = moveinput * movespeed;
+    }
+
+    void StopMoveFunc()
+    {
+        rigidbody.velocity = Vector2.zero;
+    }
+
+}
